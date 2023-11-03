3d833816a17e7655681b35f2d90ac1a2faca15e5
AshScops <1349143251@qq.com>
2023-11-03 19:32:41 +0800

    隧道整体插值变换

--- Assets/_Scripts/QuadCreator.cs
+++ Assets/_Scripts/QuadCreator.cs
@@ -5,29 +5,51 @@ using UnityEngine;
 
 public class QuadCreator : MonoBehaviour
 {
+    private enum TubeState
+    {
+        Ring = 0,
+        Plane,
+        RingToPlane,
+        PlaneToRing
+    }
+    [SerializeField]
+    private TubeState m_tubeState;
+
     public GameObject QuadPrefab;
+
     public GameObject RingPrefab;
+    private List<Vector3> RingPositions = new List<Vector3>();
+    private List<Quaternion> RingRotations = new List<Quaternion>();
+
     public GameObject PlanePrefab;
+    private List<Vector3> PlanePositions = new List<Vector3>();
+    private List<Quaternion> PlaneRotations = new List<Quaternion>();
+
+    private List<Vector3> TargetPositions = new List<Vector3>();
+    private List<Quaternion> TargetRotations = new List<Quaternion>();
+
+    public float LerpPerAdd = 0.01f;
 
     public int SumLength = 30;
-
     private int m_quadCount = 12;
     private float m_radius = 5.0f;
     public float Width => QuadPrefab.transform.localScale.x;
 
-    private List<GameObject> m_rings = new List<GameObject>();
+    private List<GameObject> m_rows = new List<GameObject>();
 
     private void Awake()
     {
         CalculateRadius();
+        CalculateTargetStateInfo(RingPrefab, ref RingPositions, ref RingRotations);
+        CalculateTargetStateInfo(PlanePrefab, ref PlanePositions, ref PlaneRotations);
 
-        for(int i = 0; i < SumLength; i++)
+        for (int i = 0; i < SumLength; i++)
         {
-            GenerateRing(new Vector3(transform.position.x, transform.position.y, transform.position.z + i * Width));
+            GenerateRow(new Vector3(transform.position.x, transform.position.y, transform.position.z + i * Width));
         }
-        
     }
 
+    #region 计算前置数据
     private void CalculateRadius()
     {
         // 根据边长计算半径
@@ -39,11 +61,48 @@ public class QuadCreator : MonoBehaviour
         m_radius = radius;
     }
 
-    private void GenerateRing(Vector3 centerPos)
+    private void CalculateTargetStateInfo(GameObject parent, ref List<Vector3> positions, ref List<Quaternion> rotations)
     {
-        GameObject ring = Instantiate(PlanePrefab, centerPos, Quaternion.identity);
-        ring.transform.SetParent(this.transform);
-        m_rings.Add(ring);
+        for (int i = 0; i < parent.transform.childCount; i++)
+        {
+            positions.Add(parent.transform.GetChild(i).position);
+            rotations.Add(parent.transform.GetChild(i).rotation);
+        }
+    }
+    #endregion
+
+    private void GenerateRow(Vector3 centerPos)
+    {
+        #region 直接生成父物体
+
+        GameObject row = null;
+        if (m_tubeState == TubeState.Ring)
+        {
+            row = Instantiate(RingPrefab, centerPos, Quaternion.identity);
+        }
+        else if (m_tubeState == TubeState.Plane)
+        {
+            row = Instantiate(PlanePrefab, centerPos, Quaternion.identity);
+        }
+        else if(m_tubeState == TubeState.RingToPlane)
+        {
+            row = Instantiate(RingPrefab, centerPos, Quaternion.identity);
+        }
+        else if (m_tubeState == TubeState.PlaneToRing)
+        {
+            row = Instantiate(PlanePrefab, centerPos, Quaternion.identity);
+        }
+
+        if(row != null)
+        {
+            row.transform.SetParent(this.transform);
+            m_rows.Add(row);
+        }
+        else
+        {
+            print("枚举值不存在");
+        }
+        #endregion
 
         #region 12个面片成环
         //float angleStep = 360.0f / m_quadCount;
@@ -71,12 +130,21 @@ public class QuadCreator : MonoBehaviour
         #endregion
     }
 
+    //根据传参设置位置和旋转
+    private void SetQuad(int index ,Vector3 position, Quaternion rotation)
+    {
+        GameObject quad = Instantiate(QuadPrefab, position, rotation);
+        quad.transform.parent = transform;
+        m_rows.Add(quad);
+    }
+
     public float MoveSpeed = 5f;
     private Queue<GameObject> m_waitDelete = new Queue<GameObject>();
     private Queue<float> m_waitGeneratePos = new Queue<float>();
+
     private void Update()
     {
-        m_rings.ForEach((ring) =>
+        m_rows.ForEach((ring) =>
         {
             //全体位移
             ring.transform.position -= new Vector3(0, 0, MoveSpeed * Time.deltaTime);
@@ -92,31 +160,89 @@ public class QuadCreator : MonoBehaviour
         //删除待删除面片
         while (m_waitDelete.Count > 0)
         {
-            GameObject ring = m_waitDelete.Dequeue();
-            m_rings.Remove(ring);
-            Destroy(ring);
+            GameObject quad = m_waitDelete.Dequeue();
+            m_rows.Remove(quad);
+            Destroy(quad);
         }
 
         //生成对应面片
         while (m_waitGeneratePos.Count > 0)
         {
             float z = m_waitGeneratePos.Dequeue();
-            GenerateRing(new Vector3(transform.position.x, transform.position.y, z));
+            GenerateRow(new Vector3(transform.position.x, transform.position.y, z));
         }
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            print("开始变换");
+            if(m_tubeState == TubeState.Ring)
+            {
+                print("Ring变换Plane");
+                SetTubeStateWithDuration(TubeState.Plane);
+            }
+            else if (m_tubeState == TubeState.Plane)
+            {
+                print("Plane变换Ring");
+                SetTubeStateWithDuration(TubeState.Ring);
+            }
+            else//TODO:环形分正反两面
+            {
 
+            }
+        }
+    }
+
+    private void SetTubeStateWithDuration(TubeState targetState)
+    {
+        if (targetState == TubeState.Ring)
+        {
+            m_tubeState = TubeState.PlaneToRing;
+            StartCoroutine(DoTubeTransform(targetState, RingPrefab));
+        }
+        else if (targetState == TubeState.Plane)
+        {
+            m_tubeState = TubeState.RingToPlane;
+            StartCoroutine(DoTubeTransform(targetState, PlanePrefab));
+        }
+        else//TODO:环形分正反两面
+        {
 
         }
     }
 
-    private void ChangeTubeShape()
+    /// <summary>
+    /// 这种方案需要Prefab中子物体的索引一一对应
+    /// </summary>
+    /// <param name="targetState"></param>
+    /// <param name="targetObj"></param>
+    /// <returns></returns>
+    private IEnumerator DoTubeTransform(TubeState targetState, GameObject targetObj)
     {
+        float m_lerpNum = 0;
+        while (m_lerpNum < 1f)
+        {
+            yield return null;
 
+            //应用在全部面片上，看起来就是隧道整体在变换
+            for (int i = 0; i < m_rows.Count; i++)
+            {
+                for (int j = 0; j < m_rows[i].transform.childCount; j++)
+                {
+                    var targetQuad = m_rows[i].transform.GetChild(j);
+                    targetQuad.localPosition =
+                        Vector3.Lerp(targetQuad.localPosition, targetObj.transform.GetChild(j).localPosition, m_lerpNum);
+                    targetQuad.localRotation =
+                        Quaternion.Lerp(targetQuad.localRotation, targetObj.transform.GetChild(j).localRotation, m_lerpNum);
+                }
+            }
+            m_lerpNum += LerpPerAdd;
+        }
 
+        m_tubeState = targetState;
+    }
 
+    private void OnGUI()
+    {
+        
     }
 
 }
